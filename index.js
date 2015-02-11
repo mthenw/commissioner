360762786a652faba1f65e68e1f369808c4dd515
Maciej Winnicki <maciej.winnicki@gmail.com>
2015-02-11 19:10:51 +0100

    consul support

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-module.exports = function(name, port, cb) {
-  var key = name.toUpperCase() + '_PORT_' + port + '_TCP_';
-  var addrKey = key + 'ADDR';
-  var portKey = key + 'PORT';
+var dns = require('dns');
 
-  cb(null, process.env[addrKey], process.env[portKey]);
+module.exports = function(name, port, cb) {
+  dns.resolveSrv(name + '.service.consul', function(err, addresses) {
+    if (err) {
+      var key = name.toUpperCase() + '_PORT_' + port + '_TCP_';
+      var addrKey = key + 'ADDR';
+      var portKey = key + 'PORT';
+
+      cb(null, process.env[addrKey], process.env[portKey]);
+    } else {
+      cb(null, addresses[0].name, addresses[0].port);
+    }
+  });
 }
