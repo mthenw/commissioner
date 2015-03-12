3fc3104de02cbabd42b7a682f9e50f29585db206
Maciej Winnicki <maciej.winnicki@gmail.com>
2015-03-12 15:38:11 +0100

    Example for docker-compose

---
+++ example/README.md
@@ -0,0 +1,7 @@
+# Scenario #1 - Docker Compose
+
+Run application using Docker Compose
+
+```
+docker-compose up
+```
---
+++ example/app.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var redis = require('redis');
+var commissioner = require('commissioner');
+
+var client;
+commissioner('db', 6379, function(err, records) {
+  client = redis.createClient(records[0].port, records[0].addr);
+});
+
+http.createServer(function(req, res) {
+  client.incr('visits');
+  client.get('visits', function(err, count) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Visited: ' + count);
+  });
+}).listen(1337);
+console.log('Server running at http://127.0.0.1:1337/');
